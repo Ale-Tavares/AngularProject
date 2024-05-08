167092c24bdec060ec7f21a2f2e127c8081f6f39
unknown <alxtvrs@hotmail.com>
2024-05-08 12:57:47 -0300

    Learning components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp2Component
+    Comp2Component,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
